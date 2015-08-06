6435a3c090c3d706f55c66235215e55fd1129962
xqf <xqf@eeepay.cn>
2015-08-06 09:17:13 +0800

    控件ontouchEvent事件

--- app/src/main/java/com/example/administrator/myapplication150512/SlidingDrawerActivity.java
+++ app/src/main/java/com/example/administrator/myapplication150512/SlidingDrawerActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.myapplication150512;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SlidingDrawer;
@@ -16,7 +17,9 @@ public class SlidingDrawerActivity extends AppCompatActivity {
     private Button btn_drawer;//抽屉的按钮
     private TempSlidingDrawer mDrawer;
     private Boolean flag = false;
-
+    Button mbtn;
+    Button mBtnHuadong;
+    boolean isShow=false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +34,12 @@ public class SlidingDrawerActivity extends AppCompatActivity {
     }
 
     private void initDrawer() {
+        mBtnHuadong=(Button)findViewById(R.id.drawer_btn);
+        testBtn();
         btn_drawer = (Button) findViewById(R.id.handle);
         mDrawer = (TempSlidingDrawer) findViewById(R.id.slidingDrawer1);
-        Button mbtn=(Button)findViewById(R.id.btn1);
+         mbtn=(Button)findViewById(R.id.btn1);
+
         mbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,5 +82,62 @@ public class SlidingDrawerActivity extends AppCompatActivity {
                 Toast.makeText(this, "ok  hahh", Toast.LENGTH_LONG).show();
                 break;
         }
+    }
+    float x=0;
+    float y=0;
+
+    /**
+     * 测试滑动手势
+     */
+    private void testBtn()
+    {
+        mBtnHuadong.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                float tempX=0;
+                float tempY=0;
+
+                switch (event.getAction())
+                {
+
+                    case MotionEvent.ACTION_DOWN:
+                        isShow=false;
+                        x=event.getX();
+                        y=event.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        tempX=event.getX();
+                        tempY=event.getY();
+                        if (!isShow)
+                        {
+                            if((tempX-x)>50)
+                            {
+                                isShow=true;
+                                Toast.makeText(SlidingDrawerActivity.this, "ok  向右滑动》》》》", Toast.LENGTH_SHORT).show();
+                            }else if((x-tempX)>50)
+                            {
+                                isShow=true;
+                                Toast.makeText(SlidingDrawerActivity.this, "ok  《《《向左滑动", Toast.LENGTH_SHORT).show();
+                            }else
+                            {
+
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+
+
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
     }
 }
